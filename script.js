7e481e83ad42cd9d923b92f1006b2fdcf1f2529a
LegoManager <137654124+LegoManager@users.noreply.github.com>
2023-07-15 23:47:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  function gitlink() {
+    window.location.href = "https://github.com/DarioNet0", "_blank"
+  } 
+  function instagramlink() {
+    window.location.href = "https://www.instagram.com/dario_uneto/"
+  } 
+  function linkedinlink() {
+    window.location.href = "https://www.linkedin.com/in/d%C3%A1rio-neto-82b62b253/"
+  } 
